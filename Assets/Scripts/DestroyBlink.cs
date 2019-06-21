fd52606fce3c5478c29d83dd92850e0ebec2ff56
ashitr <r.ashit18@gmail.com>
2019-06-21 22:32:59 +0530

    ARCore_before_menu

---
+++ Assets/Scripts/DestroyBlink.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBlink : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, 0.5f);
+	}
+}
